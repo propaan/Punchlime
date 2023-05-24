631ea034f7ab0d24b26e3810c631f096b717c774
propan <legrandenzo.pro@gmail.com>
2023-05-24 15:51:22 +0200

    Add punchlime

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
+import logo from './assets/images/punchlime.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <ul id="navbar">
+          <li><img id="nav-logo" src={logo}></img></li>
+          <li id="brand">Punchlime</li>
+          <li id="active" className="navigation">Accueil</li>
+          <li className="navigation">Classement</li>
+          <li id="connexion" className="navigation">Connexion</li>
+        </ul>
       </header>
+
+      <div id="v-body">
+        <h1 id="presentation">Trouve qui a écrit la punchline.</h1>
+        <div>
+          <button id="btn-jouer">JOUER</button>
+        </div>
+      </div>
     </div>
   );
 }
